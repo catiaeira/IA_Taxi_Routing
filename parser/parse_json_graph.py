ccb38596c757f3a776e904337c2dd0baf9d49b72
catiaeira <catia.eira71@gmail.com>
2025-11-14 14:05:24 +0000

    cálculo da distância entre nodos (haversine)

--- parser/parse_json_graph.py
+++ parser/parse_json_graph.py
@@ -1,5 +1,6 @@
 from graph.Graph import Graph
 import json
+import math
 
 def parse_graph() -> Graph:
     #file_path = input("File path: ")
@@ -10,10 +11,28 @@ def parse_graph() -> Graph:
 
     g = Graph()
 
+    nodes_dict = {node["name"]: node for node in json_graph["nodes"]}
+
     for node in json_graph["nodes"]:
-        g.add_node(node["name"], node["heuristic"], node["type"])
+        g.add_node(node["name"], 0, node["type"]) #heuristic initialized at 0
 
     for edge in json_graph["edges"]:
-        g.add_edge(edge["origin"], edge["destiny"], edge["dist"], edge["speed"])
+        origin = nodes_dict.get(edge["origin"])
+        destiny = nodes_dict.get(edge["destiny"])
+        g.add_edge(edge["origin"], edge["destiny"], dist(origin, destiny), edge["speed"])
 
     return g
+
+def dist(origin, destiny) -> float:
+    latOr = origin["latitude"] * math.pi/180        #converted from decimal degrees to radians
+    latDest = destiny["latitude"] * math.pi/180
+
+    longOr = origin["longitude"] * math.pi/180
+    longDest = destiny["longitude"] * math.pi/180
+
+    #distance in kms - Haversine Formula (rounding up)
+    distance = math.ceil(2 * 6371 * math.asin(math.sqrt(math.pow(math.sin((latDest - latOr)/2), 2) + math.cos(latOr)*math.cos(latDest)*math.pow(math.sin((longDest - longOr)/2),2))))
+
+    return distance
+
+
