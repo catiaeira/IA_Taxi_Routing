4e53232d732cb9d2276c9e7acee4f8ebe8da48e0
MarcoFerreira05 <a106857@alunos.uminho.pt>
2025-12-15 23:26:54 +0000

    Add highway type parsing

--- parser/get_osmnx_graph.py
+++ parser/get_osmnx_graph.py
@@ -6,6 +6,46 @@ import os.path as osp
 
 from graph.Graph import Graph, Energy_Station
 
+def match_single_type(road_type: str) -> int:
+    match road_type:
+        case "motorway":
+            return 120
+        case "trunk":
+            return 100
+        case "primary":
+            return 90
+        case "motorway_link" | "trunk_link" | "primary_link" | "secondary":
+            return 70
+        case "secondary_link" | "tertiary" | "busway":
+            return 50
+        case "tertiary_link" | "residential" | "unclassified" | "road":
+            return 30
+        case "living_street" | "service":
+            return 20
+        case "escape":
+            return 5
+        case _:
+            print(f"Unknown highway type detected -> {road_type}")
+            return 1
+
+
+def parse_highway(road_types: str|list[str]) -> int:
+    
+    if isinstance(road_types, str):
+        return match_single_type(road_types)
+    else: 
+        speeds: list[int] = []
+
+        for road_type in road_types:
+            speeds.append(match_single_type(road_type))
+
+        min = 120
+        for speed in speeds:
+            if speed < min:
+                min = speed
+
+        return min
+
 def get_graph() -> Graph:
 
     graph_path = "parser/osmnx.graphml"
@@ -21,8 +61,9 @@ def get_graph() -> Graph:
         graph.add_node(str(node), data['y'], data['x'], Energy_Station.NONE)
 
     max_speed = 0
+    speed = 0
     for origin, destination, data in graph_osmnx.edges(data=True):
-        speed = 50
+        speed = parse_highway(data['highway'])
         if speed > max_speed:
             max_speed = speed
         # the distances come in meters as floats, so it's safe to convert to int
